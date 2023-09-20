430ac180670f701d6810d958cdd199d20f79b150
JeetashaArora <140963524+JeetashaArora@users.noreply.github.com>
2023-09-21 01:46:06 +0530

    Create Login.jsx

---
+++ Task7.1P/Components/Login.jsx
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import app from "./utils/firebase"
+import Signup from './routes/Signup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'semantic-ui-css/semantic.min.css'
+import { Routes, Route } from 'react-router-dom';
+import Login from './routes/Login'
+import Homepage from './Homepage';
+import Navbar from './Navbar';
+import Navigation from './Navigation';
+function App() {
+  return (
+    <div >
+     <Routes>
+     <Route path='/' element={<Navigation/>}>
+     <Route index element={<Login/>}/>
+     <Route path='/signup' element={<Signup/>}/>
+     <Route path='/login' element={<Login/>}/>
+     <Route path='/home' element={<Homepage/>}/>
+     </Route>
+     </Routes>
+     </div>
+  );
+}
+export default App;
